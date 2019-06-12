4a9c255b0db91beaeb791e0cabba8cc545a54b7f
Nathan Goldbaum <nathan12343@gmail.com>
2019-06-12 15:10:35 -0400

    bugfix

--- p01/src/main.rs
+++ p01/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
-use std::collections::HashSet;
 
 fn main() -> Result<(), std::io::Error> {
     println!("Hello, world!");
@@ -22,10 +22,16 @@ fn get_contents(fname: &str) -> Result<String, std::io::Error> {
 }
 
 fn parse_contents(contents: String) -> Vec<(char, i32)> {
-    contents.split(", ").map(|x| {
-        let mut c = x.chars();
-        (c.next().unwrap(), dbg!(c.collect::<String>()).parse::<i32>().unwrap())
-    }).collect()
+    contents
+        .split(", ")
+        .map(|x| {
+            let mut c = x.chars();
+            (
+                c.next().unwrap(),
+                dbg!(c.collect::<String>()).parse::<i32>().unwrap(),
+            )
+        })
+        .collect()
 }
 
 fn navigate(directions: Vec<(char, i32)>) -> (i32, i32) {
@@ -38,29 +44,34 @@ fn navigate(directions: Vec<(char, i32)>) -> (i32, i32) {
         match (d.0, current_direction) {
             ('L', 'N') | ('R', 'S') => {
                 current_direction = 'W';
-            },
+            }
             ('R', 'N') | ('L', 'S') => {
                 current_direction = 'E';
-            },
+            }
             ('L', 'E') | ('R', 'W') => {
                 current_direction = 'N';
-            },
+            }
             ('R', 'E') | ('L', 'W') => {
                 current_direction = 'S';
-            },
-            _ => unreachable!()
+            }
+            _ => unreachable!(),
         }
         if let Some(p) = take_step(d.1, &mut position, &mut seen_positions, current_direction) {
-            return p
+            return p;
         }
     }
     position
 }
 
-fn take_step(num_steps: i32, position: &mut (i32, i32), seen_positions: &mut HashSet<(i32, i32)>, direction: char) -> Option<(i32, i32)> {
+fn take_step(
+    num_steps: i32,
+    position: &mut (i32, i32),
+    seen_positions: &mut HashSet<(i32, i32)>,
+    direction: char,
+) -> Option<(i32, i32)> {
     for _ in 0..num_steps {
         if seen_positions.contains(&position) {
-            return Some(*position)
+            return Some(*position);
         } else {
             seen_positions.insert(*position);
         }
@@ -68,17 +79,17 @@ fn take_step(num_steps: i32, position: &mut (i32, i32), seen_positions: &mut Has
         match direction {
             'N' => {
                 position.1 += 1;
-            },
+            }
             'S' => {
                 position.1 -= 1;
-            },
+            }
             'E' => {
                 position.0 += 1;
-            },
+            }
             'W' => {
                 position.0 -= 1;
-            },
-            _ => unreachable!()
+            }
+            _ => unreachable!(),
         }
     }
     None
